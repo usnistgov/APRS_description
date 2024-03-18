964f283f3e19deeab861f0b68bd55e7210dff970
jfernandez37 <jothomas0615@gmail.com>
2024-03-18 16:42:15 +0000

    Uses namespaces and runs without issue

--- launch/load_aprs_robots_ns.launch.py
+++ launch/load_aprs_robots_ns.launch.py
@@ -14,18 +14,36 @@ from ament_index_python.packages import get_package_share_directory
 
 def launch_setup(context, *args, **kwargs):
     robot_names = ["fanuc", "franka", "motoman", "ur"]
+
+    # urdf = os.path.join(get_package_share_directory('aprs_description'), 'urdf', 'aprs_lab_robots.urdf.xacro')
+    
+    # doc = xacro.process_file(urdf)
+
+    # robot_description_content = doc.toprettyxml(indent='  ')
+
+    # print(robot_description_content)
+
+    # robot_description = {"robot_description": robot_description_content}
+
     robot_urdf_docs = {name:xacro.process_file(os.path.join(get_package_share_directory('aprs_description'), 'urdf', f'aprs_{name}.urdf.xacro')) for name in robot_names}
     robot_descriptions = {name:{"robot_description":robot_urdf_docs[name].toprettyxml(indent='  ')} for name in robot_names}
 
-    robot_state_publishers = {}
-    for name in robot_names:
-        robot_state_publishers[name] = Node(
+    robot_state_publishers = []
+    for robot_name in robot_names:
+        robot_state_publishers.append(Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
-            namespace=f"{name}_publisher",
+            namespace=f"{robot_name}",
             output="both",
-            parameters=[{"use_sim_time": True}, robot_descriptions[name]],
-        )
+            parameters=[{"use_sim_time": True}, robot_descriptions[robot_name]],
+        ))
+
+    # robot_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     output="both",
+    #     parameters=[{"use_sim_time": True}, robot_description],
+    # )
 
     world_path = os.path.join(get_package_share_directory('aprs_description'), 'worlds', 'lab.sdf')
     
@@ -35,76 +53,47 @@ def launch_setup(context, *args, **kwargs):
                               'launch', 'gz_sim.launch.py')]),
             launch_arguments=[('gz_args', [' -r -v 4 '+ world_path])])
 
-    gz_spawners = {}
-    for name in robot_names:
-        gz_spawners[name] = Node(
+    gz_spawners = []
+    for robot_name in robot_names:
+        gz_spawners.append(Node(
             package='ros_gz_sim',
             executable='create',
             output='screen',
-            namespace=f"{name}_spawner",
-            arguments=['-string', robot_urdf_docs[name].toxml(),
-                    '-name', f'aprs_{name}',
+            namespace=f"{robot_name}",
+            name=f"{robot_name}_spawner",
+            arguments=['-string', robot_urdf_docs[robot_name].toxml(),
+                    '-name', f'aprs_{robot_name}',
                     '-allow_renaming', 'true'],
-        )
+        ))
+
+    # gz_spawn_entity = Node(
+    #     package='ros_gz_sim',
+    #     executable='create',
+    #     output='screen',
+    #     arguments=['-string', doc.toxml(),
+    #                '-name', 'aprs_robots',
+    #                '-allow_renaming', 'true'],
+    # )
     
-    joint_state_broadcaster = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="joint_state_broadcaster_spawner",
-        arguments=["joint_state_broadcaster"],
-        parameters=[
-            {"use_sim_time": True},
-        ],
-    )
-    
-    ur_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="ur_controller_spawner",
-        arguments=["ur_joint_trajectory_controller"],
-        parameters=[
-            {"use_sim_time": True},
-        ],
-    )
-    
-    fanuc_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="fanuc_controller_spawner",
-        arguments=["fanuc_joint_trajectory_controller"],
-        parameters=[
-            {"use_sim_time": True},
-        ],
-    )
-    
-    franka_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="franka_controller_spawner",
-        arguments=["franka_joint_trajectory_controller"],
-        parameters=[
-            {"use_sim_time": True},
-        ],
-    )
-    
-    motoman_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        name="motoman_controller_spawner",
-        arguments=["motoman_joint_trajectory_controller"],
-        parameters=[
-            {"use_sim_time": True},
-        ],
-    )
+    controller_spawner_nodes = []
+    for robot_name in robot_names:
+        controller_spawner_nodes.append(Node(
+            package="controller_manager",
+            executable="spawner",
+            name=f"controller_manager",
+            namespace=f"{robot_name}",
+            arguments=[f"{robot_name}_controller"],
+            parameters=[
+                {"use_sim_time": True},
+            ],
+        ))
 
     nodes_to_start = [
         gz,
-        joint_state_broadcaster,
-        ur_controller,
-        fanuc_controller,
-        franka_controller,
-        motoman_controller
-    ] + [robot_state_publishers[name] for name in robot_names] + [gz_spawners[name] for name in robot_names]
+        *controller_spawner_nodes,
+        *robot_state_publishers,
+        *gz_spawners
+    ]
 
     return nodes_to_start
 
